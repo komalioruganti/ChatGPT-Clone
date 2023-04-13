10d27d035c5e23055acf9813878d655fa3d7010b
komali <95839946+komalioruganti@users.noreply.github.com>
2023-04-13 18:03:23 +0530

    Designed the Side Menu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+return ( <div className="App">
+    <aside className="sidemenu"></aside>
+    <section className="chatsection">
+    <div className="chat-log">
+        Messages
+    </div>
+    </section>
+</div>
+)
+}
+export default App;
